80adffc224f842940e00e6637b3bb2def0b2f689
SATHYAMOORTHY TS <sathyamoortthyts0105@gmail.com>
2024-08-25 16:44:25 +0530

    Advanced Data types

---
+++ Ex_12062024/Program - Lab0036.py
@@ -0,0 +1,17 @@
+# Problem to find the max between two numbers
+
+a = int(input("Enter the first number\n: "))
+b = int(input("Enter the second number\n: "))
+
+# Max
+
+result = max(a, b)
+print(result)
+
+if  a > b:
+    print(a)
+
+else:
+    print(b)
+
+    

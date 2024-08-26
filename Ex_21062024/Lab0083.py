b48eb612a8326d3213e426b0e15d995eb5bbbe5b
SATHYAMOORTHY TS <sathyamoortthyts0105@gmail.com>
2024-08-26 20:07:24 +0530

    Tuples

--- Ex_21062024/Lab0083.py
+++ Ex_21062024/Lab0083.py
@@ -1,12 +1,25 @@
-x = 10
-q, w, e = (23, 22, 33,)
+# x = 10
+# q, w, e = (23, 22, 33,)
+#
+# print(x)
+# print(q)
+# print(w)
+#
+#
+# # search in tuple
+# tuple1 = ("london",  "paris", "tokyo", "new york")
+# print("paris" in tuple1)
+# print("shanghai" in tuple1)
 
-print(x)
-print(q)
-print(w)
+t = (1,2,3,4,5,)
+#t.append() tuple are immutable in nature
+new_t = t + (11,) # append - we can add another element in the tuple
+print(new_t)
 
-
-# search in tuple
-tuple1 = ("london",  "paris", "tokyo", "new york")
-print("paris" in tuple1)
-print("shanghai" in tuple1)
+my_list = list(t)
+print(my_list)
+my_list.append(11)
+my_list.append(22)
+print(my_list)
+new_t2 = tuple(my_list)
+print(new_t2)
